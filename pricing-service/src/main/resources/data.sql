545a705823306fe621030d45d2c3d98d28abcc49
Swathi <swathisheshadri1996@gmail.com>
2023-02-17 15:10:32 +0530

    Adding src file

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO price (vehicle_id, currency, price) VALUES (1, 'USD', 12400);
+INSERT INTO price (vehicle_id, currency, price) VALUES (2, 'USD', 252400);
+INSERT INTO price (vehicle_id, currency, price) VALUES (3, 'USD', 70780);
